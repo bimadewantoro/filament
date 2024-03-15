7b0e03a831a7d7042154fafa608a14e51acb36f9
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-16 05:20:56 +0700

    feat: [BD] styling homepage (WIP)

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import {FwbCard, FwbDropdown, FwbListGroup, FwbListGroupItem} from "flowbite-vue";
 </script>
 
 <template>
@@ -12,8 +13,46 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="overflow-hidden sm:rounded-lg">
+                    <h1 class="text-3xl mb-3">
+                        Courses
+                    </h1>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                        <fwb-card>
+                            <div class="p-5">
+                                <h5 class="mb-2 flex justify-between text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Test
 
+                                    <div>
+                                        <fwb-dropdown v-if="$page.props.auth.user">
+                                            <template #trigger>
+                                                <span>
+                                                    <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                                                    </svg>
+                                                </span>
+                                            </template>
+                                            <fwb-list-group>
+                                                <fwb-list-group-item>
+                                                    <a href="#">
+                                                        Edit Course
+                                                    </a>
+                                                </fwb-list-group-item>
+                                                <fwb-list-group-item class="bg-red-500 border-red-500 text-gray-100">
+                                                    <a href="#">
+                                                        Delete Course
+                                                    </a>
+                                                </fwb-list-group-item>
+                                            </fwb-list-group>
+                                        </fwb-dropdown>
+                                    </div>
+                                </h5>
+                                <p class="font-normal text-gray-900 dark:text-gray-400">
+                                    Test
+                                </p>
+                            </div>
+                        </fwb-card>
+                    </div>
                 </div>
             </div>
         </div>

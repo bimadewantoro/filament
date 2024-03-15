1883ce180a83b288090554db6a4e489075bd83ad
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-15 14:07:47 +0700

    feat: [BD] layouting homepage

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,10 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+})->name('home');

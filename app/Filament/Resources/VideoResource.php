f83b330aa847eb63ce1e29f0627454f1b659744b
Bima Dewantoro <bimadewantoro22@gmail.com>
2024-03-21 11:35:57 +0700

    feat: [BD] Video Resource

---
+++ app/Filament/Resources/VideoResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VideoResource\Pages;
+use App\Filament\Resources\VideoResource\RelationManagers;
+use App\Models\Video;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VideoResource extends Resource
+{
+    protected static ?string $model = Video::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('url')->url()->required(),
+                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\TextInput::make('description')->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('url')->searchable(),
+                Tables\Columns\TextColumn::make('title')->searchable(),
+                Tables\Columns\TextColumn::make('video')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVideos::route('/'),
+            'create' => Pages\CreateVideo::route('/create'),
+            'edit' => Pages\EditVideo::route('/{record}/edit'),
+        ];
+    }
+}
